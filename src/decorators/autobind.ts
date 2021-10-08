5f01f09d1504c11d2810bbdb4c6debaed81fab8a
Brant Hajek <branthajek@gmail.com>
2021-10-07 22:42:07 -0700

    add comment

--- src/decorators/autobind.ts
+++ src/decorators/autobind.ts
@@ -1,4 +1,6 @@
 // Auto-bind decorator
+// This is an example of a custom decorator, and how it could be applied.
+// Binding "this" is no longer a necessary pattern with ES6 functions, but I think it's cool nonetheless
 export function AutoBind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
     const adjustedDescriptor: PropertyDescriptor = {
